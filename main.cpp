c6294b322c0f1a02cc9e6d5cfdaab8bd1200351d
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-03-03 12:38:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -389,8 +389,8 @@ extern "C" void* ThreadStats(void*) {
   } while(1);
 }
 
-static const string mainnet_seeds[] = {"dnsseed.btcprivate.org" , "btcp.suprnova.cc", "suprnova.cc", ""};
-static const string testnet_seeds[] = {"35.189.27.109", "54.91.108.114", "165.227.6.212", "136.33.111.57", "173.212.196.73", "46.242.129.78", "172.105.198.176", "45.32.223.253", "dnsseed.testnet1.btcprivate.org", ""};
+static const string mainnet_seeds[] = {"dnsseed.btcprivate.org" , "btcp.suprnova.cc", "suprnova.cc", "explorer.btcprivate.org", "35.197.238.240", "35.196.199.50", "35.173.47.70", "35.205.135.42", "35.203.33.26", "35.226.23.116", ""};
+static const string testnet_seeds[] = {"dnsseed.testnet1.btcprivate.org", ""};
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
